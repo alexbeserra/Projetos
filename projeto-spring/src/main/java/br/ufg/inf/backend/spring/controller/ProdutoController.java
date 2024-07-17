cbc04ab8fbde66a629e1430df4c923e016f691d9
alexbeserra <alexbeserrab@gmail.com>
2024-07-17 17:31:57 -0300

    Inclusão do repository e service

--- projeto-spring/src/main/java/br/ufg/inf/backend/spring/controller/ProdutoController.java
+++ projeto-spring/src/main/java/br/ufg/inf/backend/spring/controller/ProdutoController.java
@@ -1,27 +1,25 @@
 package br.ufg.inf.backend.spring.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.ufg.inf.backend.spring.model.Produto;
+import br.ufg.inf.backend.spring.service.ProdutoService;
 
 @Controller
 public class ProdutoController {
-	private List<Produto> produtos = new ArrayList<Produto>(Arrays.asList(
-			new Produto(1L, "Notebook", 2000.00),
-			new Produto(2L, "Smartphone", 1000.00))
-	);
-	
+	@Autowired
+	private ProdutoService produtoService;
+
 	@GetMapping("/produtos")
-	public String listarProdutos(Model model) {
-		model.addAttribute("produtos", produtos);
+	public String listarProdutos(Model model, @RequestParam(required = false) String sucesso) {
+		model.addAttribute("produtos", produtoService.listarProdutos());
+		model.addAttribute("sucesso", sucesso);
 		return "produtos";
 	}
 
@@ -31,15 +29,13 @@ public class ProdutoController {
 	}
 
 	@PostMapping("/produtos")
-	public String adicionarProduto(@RequestParam String nome, @RequestParam double preco, Model model) {
-		// Simulando a adição de um produto ao banco de dados
+	public String adicionarProduto(@RequestParam String nome, @RequestParam double preco,
+			RedirectAttributes redirectAttributes) {
 		Produto produto = new Produto();
 		produto.setNome(nome);
 		produto.setPreco(preco);
-		produtos.add(produto);
-		// Adicionando o produto à lista de produtos
-		model.addAttribute("produtos", produtos);
-		model.addAttribute("sucesso", "Produto adicionado com sucesso!");
-		return "produtos";
+		produtoService.salvarProduto(produto);
+		redirectAttributes.addAttribute("sucesso", "Produto adicionado com sucesso!");
+		return "redirect:/produtos";
 	}
 }
--- projeto-spring/src/main/java/br/ufg/inf/backend/spring/model/Produto.java
+++ projeto-spring/src/main/java/br/ufg/inf/backend/spring/model/Produto.java
@@ -1,6 +1,14 @@
 package br.ufg.inf.backend.spring.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private double preco;
