327e6184e66034b95dfff1c79ae632cfdba7c305
alexbeserra <alexbeserrab@gmail.com>
2024-07-20 20:16:43 -0300

    Criacao do novo projeto para realizar a tarefa que inclui categoria e tag

---
+++ projeto-spring-cat-tag/src/test/java/br/ufg/inf/backend/spring_cat_tag/ProjetoSpringCatTagApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufg.inf.backend.spring_cat_tag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringCatTagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
