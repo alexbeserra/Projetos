a0ab352e6528020b60d0a4ec8f3e9dd196408af4
alexbeserra <alexbeserrab@gmail.com>
2024-07-22 16:03:10 -0300

    Criacao da classe categoria

---
+++ projeto-spring-cat-tag/src/main/java/br/ufg/inf/backend/spring_cat_tag/model/Categoria.java
@@ -0,0 +1,24 @@
+package br.ufg.inf.backend.spring_cat_tag.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	@OneToMany(mappedBy = "categoria")
+	private List<Produto> produtos;
+}
